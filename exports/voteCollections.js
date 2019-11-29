7654977d7517274159fe0e228b1eccbf5c755a96
Syrinx <faujoux@gmail.com>
2019-11-29 23:36:45 +0100

    New function for collected votes

--- exports/voteCollections.js
+++ exports/voteCollections.js
@@ -1,20 +1,47 @@
 const fs = require("fs")
+const emojiTable = {};
+emojiTable[VotesEmojis[0]] = "oui";
+emojiTable[VotesEmojis[1]] = "non";
+emojiTable[VotesEmojis[2]] = "blanc";
+emojiTable[VotesEmojis[3]] = "delai";
+
 
 function filterReactions(expectedEmojis) {
   return (reaction, user) => {return (!user.bot) && (expectedEmojis.includes(reaction.emoji.name))}
 }
-function updateVotes(reaction, msg) {
-  let votesJSON = JSON.parse(fs.readFileSync(VotesFile))
-  msg.reactions
-  votesJSON[msg.id].votes.push()
-  fs.writeFileSync("private/votes.json", JSON.stringify(votesJSON, null, 2))
-  TiCu.Log.Commands.Vote.Anon(type, params, newMsg, msg)
+function updateVotes(reaction) {
+  let votesJSON = JSON.parse(fs.readFileSync(VotesFile));
+  let msg = reaction.message;
+  let user;
+  for (const id of reaction.users.keyArray()) {
+    if (id !== PUB.tipouitaculte) {
+      user = id;
+      reaction.remove(user);
+      break;
+    }
+  }
+  let alreadyVoted;
+  for (const emojiType of Object.values(emojiTable)) {
+    if (votesJSON[msg.id].votes[emojiType].includes(user)) {
+      alreadyVoted = emojiType;
+    }
+  }
+  if (alreadyVoted) {
+    votesJSON[msg.id].votes[alreadyVoted].splice(
+        votesJSON[msg.id].votes[alreadyVoted].indexOf(user),
+        1
+    );
+  }
+  votesJSON[msg.id].votes[emojiTable[reaction.emoji.name]].push(user);
+  fs.writeFileSync(VotesFile, JSON.stringify(votesJSON, null, 2));
+  //TODO LOGS sur les collected votes
+  //TiCu.Log.Commands.Vote.Anon(type, params, newMsg)
 }
 
 function createCollector(type, msg) {
   TiCu.VotesCollections.Collectors[msg.id] = msg.createReactionCollector(filterReactions(VotesEmojis));
   TiCu.VotesCollections.Collectors[msg.id].on("collect", (reaction) =>
-      TiCu.VotesCollections.Collected(type, reaction, msg))
+      TiCu.VotesCollections.Collected(type, reaction))
   TiCu.VotesCollections.Collectors[msg.id].on("end", (reaction, reason)  =>
       TiCu.VotesCollections.Done(type, reaction, reason, msg))
 }
@@ -28,13 +55,15 @@ module.exports = {
     for (const [id, entry] of Object.entries(votesJSON)) {
       if (typeof entry === "object") {
         tipoui.channels.get(entry.chan).fetchMessage(id).then(msg => {
+          //TODO LOGS sur le startup
           createCollector(entry.type, msg);
-      })
+        })
       }
     }
   },
   Collectors : {},
-  Collected : (type, reaction, msg) => {
+  Collected : (type, reaction) => {
+      updateVotes(reaction);
       return maxilog.send("collected " + type)
   },
   Done : (type, reaction, reason, msg) => {
