4157fdbf2e85ffc493fb5c71c06b29a6f60d1687
Syrinx <faujoux@gmail.com>
2020-01-14 23:02:14 +0100

    New Command to tell that a command is new

---
+++ exports/commands/new.js
@@ -0,0 +1,40 @@
+module.exports = {
+  alias: [
+    "new"
+  ],
+  activated: true,
+  authorizations : {
+    chans : {
+      type: "whitelist",
+      list: [PUB.salons.debug.id]
+    },
+    auths : {
+      type: "whitelist",
+      list: devTeam
+    },
+    roles : {
+      type: "any"
+    },
+    name : "Nouveauté",
+    desc : "Annoncer une nouvelle fonctionnalité",
+    schema : "!new <command> (idMessageWhiteboard)"
+  },
+  run : function(params, msg) {
+    if (params.length < 1) {
+      TiCu.Log.Error("new", "mauvais paramètres", msg)
+    } else {
+      const botChannel = tipoui.channels.get(PUB.salons.debug.id)
+      if (TiCu.Commands[params[0]] || TiCu.Auto[params[0]] || TiCu.Reactions[params[0]]) {
+        botChannel.send(`Une nouvelle fonctionnalité est disponible pour TipouiTaCulte !`)
+        if (params[1]) {
+          tipoui.channels.get(PUB.salons.whiteboard.id).fetchMessage(params[1]).then(
+            message => botChannel.send(`Cette fonctionnalité a été proposée à la communauté sous les termes suivants :\n"${message.embeds[0].description}"`)
+          )
+        }
+        TiCu.Commands.help.run([params[0]], botChannel.lastMessage)
+      } else {
+        TiCu.Log.Error("new", "commande introuvable", msg)
+      }
+    }
+  }
+}
