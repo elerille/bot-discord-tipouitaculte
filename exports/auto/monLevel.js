37b9a7f4d49728eae22ca27d13ae46a35d61b2e9
Syrinx <faujoux@gmail.com>
2020-05-09 14:21:52 +0200

    Fix for systems... Should have foresee this :sob:

--- exports/auto/monLevel.js
+++ exports/auto/monLevel.js
@@ -4,7 +4,7 @@ module.exports = {
   name : "Mon level ?",
   desc : "wesh læ bot, c'est quoi mon level ?\n",
   schema: "mon level ?",
-  trigger: /mon (level|niveau)\s*\?/,
+  trigger: /(mon|notre) (level|niveau)\s*\?/,
   authorizations : TiCu.Authorizations.getAuth("auto", "monlevel"),
   run : function(msg) {
     TiCu.Commands.level.run([], msg)
