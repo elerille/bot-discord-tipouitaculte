48dc9c7beaba31686ce3a1b18450c06b6dcb4ba8
Syrinx <faujoux@gmail.com>
2020-05-11 15:41:41 +0200

    Update description

--- exports/commands/ban.js
+++ exports/commands/ban.js
@@ -13,7 +13,7 @@ module.exports = {
     if(TiCu.Mention(params[0])) {target = TiCu.Mention(params[0])} else return TiCu.Log.Error("ban", "cible invalide", msg)
     let reason = !!params[1]
     if(reason) {reason = msg.content.substring(msg.content.match(crop)[1].length)}
-    msg.reply(`voulez-vous bannir <@${target.id}> du serveur ?\n👌 : Bannir en supprimant les messages des dernières 24h\n👍 : Bannir sans supprimer les messages\n👎 : Annuler`)
+    msg.reply(`voulez-vous bannir <@${target.id}> du serveur ?\n👌 : Bannir en supprimant les messages des derniers 7j\n👍 : Bannir sans supprimer les messages\n👎 : Annuler`)
       .then(newMsg => {
         newMsg
         .react("👍")
