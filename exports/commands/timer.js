86f0e7b9c99ee4dad42ff0a21f808854d4a01f98
Syrinx <faujoux@gmail.com>
2020-05-12 15:01:19 +0200

    Cannot launch two timers at the same time but can stop the current one

--- exports/commands/timer.js
+++ exports/commands/timer.js
@@ -17,34 +17,31 @@ function getMulFromUnitee(unitee, msg) {
 }
 
 function waitFor(duree, msg) {
-  if (duree > 0) {
-    if (duree > 120) {
-      setTimeout(function () {
+  if (timerOn) {
+    if (duree > 0) {
+      if (duree === 120) {
         msg.channel.send("Il reste 2 minutes")
-        waitFor(120, msg)
-      }, (duree - 120) * 1000)
-    } else if (duree > 60) {
-      setTimeout(function () {
+      } else if (duree === 60) {
         msg.channel.send("Il reste 60 secondes")
-        waitFor(60, msg)
-      }, (duree - 60) * 1000)
-    } else if (duree > 5) {
-      setTimeout(function () {
+      } else if (duree === 5) {
         msg.channel.send("Il reste 5 secondes")
-        waitFor(5, msg)
-      }, (duree - 5) * 1000)
-    } else {
+      } else if (duree < 5) {
+        msg.channel.send(duree)
+      }
       setTimeout(function () {
-        if (duree !== 1) {
-          msg.channel.send(duree - 1)
-        }
         waitFor(duree - 1, msg)
       }, 1000)
+    } else {
+      msg.channel.send("Fin du compte à rebours")
+      timerOn = false
     }
   } else {
-    msg.channel.send("Fin du timer")
+    const nbMinutes = Math.floor(duree / 60)
+    msg.channel.send(`Arrêt manuel du compte à rebours (restait ${nbMinutes > 0 ? nbMinutes + " minutes et " : ""}${duree % 60} secondes)`)
   }
 }
+
+let timerOn = false
 
 module.exports = {
   alias: [
@@ -53,37 +50,44 @@ module.exports = {
   activated: true,
   name : "Timer",
   desc : "Lance un compte à rebours",
-  schema : "!<timer> <duree> (m|min|minutes|s|sec|secondes)",
+  schema : "!timer <duree> (m|min|minutes|s|sec|secondes)\nou\n!timer stop",
   authorizations : TiCu.Authorizations.getAuth("command", "timer"),
   run : function(params, msg) {
     if (params.length <1) {
       TiCu.Commands.help.run([this.alias[0], "paramètres invalides"], msg)
     } else {
-      const rewroteParam = params[0].replace(",", ".")
-      let unit = "minutes"
-      let duree = rewroteParam
-      if (params.length === 1) {
-        const regEx = /([0-9.]+)(m|min|minutes?|s|sec|secondes?)?/i
-        const parsedParam = rewroteParam.match(regEx)
-        if (parsedParam) {
-          duree = parsedParam[1]
-          if (parsedParam[2] !== undefined) {
-            unit = parsedParam[2]
+      if (params[0] === "stop") {
+        timerOn = false
+      } else {
+        const rewroteParam = params[0].replace(",", ".")
+        let unit = "minutes"
+        let duree = rewroteParam
+        if (params.length === 1) {
+          const regEx = /([0-9.]+)(m|min|minutes?|s|sec|secondes?)?/i
+          const parsedParam = rewroteParam.match(regEx)
+          if (parsedParam) {
+            duree = parsedParam[1]
+            if (parsedParam[2] !== undefined) {
+              unit = parsedParam[2]
+            }
           }
+        } else {
+          unit = params[1]
         }
-      } else {
-        unit = params[1]
-      }
-      if (isNaN(duree)) {
-        TiCu.Log.Error(this.alias[0], `la durée "${duree}" n'est pas un nombre`, msg)
-      } else {
-        const totalTime = getMulFromUnitee(unit, msg) * Number(duree)
-        if (totalTime > 0) {
-          if (totalTime > 1800) {
-            TiCu.Log.Error(this.alias[0], `la durée maximale du timer est de 30 minutes.`, msg)
-          } else {
-            msg.channel.send(`Début du timer de ${duree} ${unit}`)
-            waitFor(totalTime, msg)
+        if (isNaN(duree)) {
+          TiCu.Log.Error(this.alias[0], `la durée "${duree}" n'est pas un nombre`, msg)
+        } else {
+          const totalTime = getMulFromUnitee(unit, msg) * Number(duree)
+          if (totalTime > 0) {
+            if (totalTime > 1800) {
+              TiCu.Log.Error(this.alias[0], `la durée maximale du compte à rebours est de 30 minutes.`, msg)
+            } else if (timerOn) {
+              msg.channel.send(`Un compte à rebours a déjà été lancé`)
+            } else {
+              msg.channel.send(`Début du compte à rebours de ${duree} ${unit}`)
+              timerOn = true
+              waitFor(totalTime, msg)
+            }
           }
         }
       }
